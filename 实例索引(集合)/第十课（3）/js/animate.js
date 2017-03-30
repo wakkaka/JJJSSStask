b775eaf1cd113f0d412c8e7825ccbe70cc34a2bc
wakkaka <zhaoyue199354@qq.com>
2017-03-30 09:40:51 +0800

    task3

---
+++ 实例索引(集合)/第十课（3）/js/animate.js
@@ -0,0 +1,44 @@
+var Animate = function(oElement,options,callback){
+	this.initialize.apply(this,arguments);
+}
+
+Animate.prototype = {
+	//初始化一个运动实例
+	initialize : function(oElement,options,callback){
+		var that = this;
+		this.oElement = typeof oElement === 'string' ? document.querySelector('.' + oElement) : oElement;
+		this.options = options;
+		this.callback = callback;
+		clearInterval(this.timer);
+		this.timer = setInterval(function(){
+			that.doMove();
+		},30);
+	},
+
+	//更改相应样式
+	css : function(attr,value){
+		if(arguments.length == 1){
+			//一个参数时，返回attr目前的值
+			//ie支持currentStyle，其他支持getComputedStyle
+			return parseFloat(this.oElement.currentStyle ? this.oElement.currentStyle[attr] : getComputedStyle(this.oElement,null)[attr]);
+		}else{
+			//两个参数时，修改attr为value
+			attr == "opacity" ? (this.oElement.style.filter = "alpha(opacity=" + value + ")", this.oElement.style.opacity = value / 100)  : this.oElement.style[attr] = value + 'px';
+		}
+	},
+
+	doMove:function(){
+		var opt = this.options;
+		var bComplete = true;//判断当前一个运动是否已经达到目标位置
+		for(var p in opt){
+			var iCur = (p == 'opacity') ? parseInt(this.css(p).toFixed(2)*100) : this.css(p);
+			console.log(p,iCur);
+			var iSpeed = (opt[p] - iCur)/5;
+			iSpeed = iSpeed>0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
+
+			(opt[p] == iCur) || (bComplete = false, this.css(p,iCur+iSpeed));
+		}
+		bComplete && ((clearInterval(this.timer)), this.callback && this.callback(this));
+
+	}
+}
---
+++ 实例索引(集合)/第十课（3）/js/index.js
@@ -0,0 +1,40 @@
+window.onload = function(){
+	var oSpan = document.getElementsByTagName('span')[0];
+	var oInpute = document.getElementsByTagName('input')[0];
+	var iFlag = true;
+	var i=0;
+	var iAction = [
+		{width:20, height:20},
+		{width:80, height:80},
+		{left:10}, 
+		{left:408},
+		{opacity:100},
+		{opacity:0},
+		{opacity:100},
+		{width:80, height:80, left:408},
+		{top:10},
+		{width:20, height:20, left:468},
+		{top:70},
+		{left:10},
+		{top:10},
+		{left:468},
+		{width:20, height:20, left:468},
+		{width:80, height:80, left:408}
+	]
+
+	oInpute.onclick = function(){
+		var that = this;
+		function begin(){
+			console.log(i);
+			iFlag ? i++ : i--;
+			var animate = new Animate(oSpan, iAction[i], begin);
+			if(i == iAction.length || i<0){
+				clearInterval(animate.timer);
+				iFlag = !iFlag;
+				that.value = iFlag ? "开始" : "原路返回";
+				return
+			}
+		}
+		begin();
+	}
+}
